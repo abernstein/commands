67e804bc7ec2205b4bb6e82b0b3b9e84131e870a
Aaron Bernstein <aaronbernstein@cox.net>
2013-12-12 17:23:23 -0700

    Update git_cli_cmds.sh

--- linux/git_cli_cmds.sh
+++ linux/git_cli_cmds.sh
@@ -3,11 +3,25 @@ git log --stat -3
 git log --pretty=oneline <COMMIT1>..<COMMIT2>
 git log --oneline --name-status <COMMIT>..<COMMIT>~8
 
+git log -S[search term]
+
+# GIT Lost Commits
+git log -g
+git branch lost_data [SHA1]
+git merge lost_data
+
+git fsck --full
+
 # List Git Config Values
 git-config -l
 
 # Clone bare empty project
 git clone --bare <REPO>
+
+# Clone a specific remote branch
+git init  
+git remote add -t BRANCH_NAME_HERE -f origin REMOTE_REPO_URL_PATH_HERE  
+git checkout BRANCH_NAME_HERE
 
 # Crush Last Three Commits
 git reset --soft HEAD~3; git commit
@@ -15,12 +29,21 @@ git reset --soft HEAD~3; git commit
 # Create a git alias
 git config --global alias.<NAME> "<CODE>"
 
+# Autocorrect GIT Commands
+git config --global help.autocorrect 1
+
 # Turn off SSL Verification (SHOULD ONLY BE USED WHEN NEEDED AND REVERTED)
 git config --global http.sslVerify false
 
 # Create an archive of a remote branch
 git archive --format=tar.gz --remote=ssh://<REPO> <TAG OR BRANCH> > <FILENAME>.tgz
 git archive --format=tar.gz --remote=git@<REPO> <TAG OR BRANCH> -o <FILENAME>.tgz
+
+# Export changes done in last commit
+git archive -o ../updated.zip HEAD $(git diff --name-only HEAD^)
+
+# Export changed files between two commits
+git archive -o ../latest.zip NEW_COMMIT_ID_HERE $(git diff --name-only OLD_COMMIT_ID_HERE NEW_COMMIT_ID_HERE)
 
 # Create a new tag
 git tag -a <SEM_VER> -m "<MESSAGE>"
@@ -92,6 +115,11 @@ git show-branch <BRANCH1> <BRANCH2>
 # Apply patches using the auto-merge tool
 git am
 
+# Apply patch from Unrelated local repository
+git --git-dir=PATH_TO_OTHER_REPOSITORY_HERE/.git format-patch -k -1 --stdout COMMIT_HASH_ID_HERE| git am -3 -k
+
+
+
 # Review the contents of a commit
 git cat-file commit HEAD
 git cat-file commit <COMMIT>..<COMMIT>~8
@@ -106,6 +134,8 @@ git help config
 git rebase master
 git rebase --continue or --skip or --abort
 
+git rebase [-i] --no-ff <COMMIT>
+
 # Add a specific set of folders/files
 git add tests/{application,library}/*
 
@@ -114,6 +144,27 @@ git revert -m 1 <COMMIT>
 git cherry-pick <COMMIT1>...<COMMIT2>
 git cherry-pick <COMMIT1>..<COMMIT2>
 git cherry-pick --abort
+
+# Check if your Branch changes are part of Other branch
+git cherry -v OTHER_BRANCH_NAME_HERE
+#For example: to check with master branch
+git cherry -v master
+
+# Start a new Branch with No History
+git checkout --orphan NEW_BRANCH_NAME_HERE
+
+# Checkout File from Other Branch without Switching Branches
+git checkout BRANCH_NAME_HERE -- PATH_TO_FILE_IN_BRANCH_HERE
+
+# Ignore Changes in a Tracked File
+git update-index --assume-unchanged PATH_TO_FILE_HERE
+
+# Check if committed changes are part of a release
+git name-rev --name-only COMMIT_HASH_HERE
+
+# Pull with rebase instead of merge
+git pull --rebase
+git config branch.BRANCH_NAME_HERE.rebase true
 
 # cleanup
 git gc --prune=now
