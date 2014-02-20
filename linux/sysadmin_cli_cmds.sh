e55c8a51fbccc2a8437a4f216f5fddb2e5b592d9
Aaron Bernstein <aaronbernstein@cox.net>
2014-02-20 16:42:05 -0700

    Update sysadmin_cli_cmds.sh

--- linux/sysadmin_cli_cmds.sh
+++ linux/sysadmin_cli_cmds.sh
@@ -31,10 +31,6 @@ tail -f /var/log/syslog
 # Find only the differences
 diff -C0 <FILE1> <FILE2> | grep -e "^\!"
 
-# Find all running services
-sudo /sbin/service --status-all | grep -i "running"
-/sbin/chkconfig --list | grep 3:on
-
 # Find list of all service and standard port numbers and protocol types
 less /etc/services
 grep <PORT> /etc/services
@@ -88,9 +84,6 @@ curl -v <URL> -d <DATA>
 # Search man pages
 man -k <SERVICE>
 
-# Status of Memcache
-memstat --servers=<SERVERS>
-
 # Find all symlinks and where they point
 symlinks -vr / 
 readlink <SYMLINK>
@@ -109,12 +102,6 @@ ssh-keygen -f <KEY FILE> -p
 
 # Show current apache config
 httpd -S
-
-# Check Open Files by Service
-lsof | awk '{print $1}' | sort | uniq -c | sort -rn | head
-
-# Determine running time for command `cp`
-time cp <filename> <new filename>
 
 # Evaulate the local likewise cache for user
 /opt/likewise/bin/lw-find-user-by-name --level 2 <username>
@@ -197,12 +184,6 @@ wget --spider <URL>
 getfacl *
 setfacl -m u:bozo:rw <FILE>
 
-# Check stats of Memcache
-echo "stats" | nc localhost 11211
-echo "stats settings" | nc localhost 11211
-echo "stats items" | nc localhost 11211
-echo "stats cachedump <SLAB CLASS> <NUMBER OF ITEMS>" | nc localhost 11211
-
 # Re-play command
 watch <OPTIONS> <COMMAND>
 
@@ -221,25 +202,3 @@ sort <SOURCE> | uniq –cd
 
 # List the #N oldest files in folder
 ls -ltr <FOLDER> | head -<N>
-
-# Current Usage Status of Memory
-free <OPTIONS>
-
-# Collect, report, or save system activity information.
-# http://www.thegeekstuff.com/2011/03/sar-examples/
-sar
-
-# Virtual Memory, I/O, Disk Stats
-vmstat
-iostat
-mpstat
-
-# Make a Password
-mkpasswd -l <LENGTH>
-
-# List Processes in a Hierarchy
-ps -e -o pid,args --forest
-pstree
-
-# List all threads for a particular process
-ps -C java -L -o pid,tid,pcpu,state,nlwp,args
