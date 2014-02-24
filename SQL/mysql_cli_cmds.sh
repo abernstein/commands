a0756242ecec77393bd1dcb0ba824a610377af86
Aaron Bernstein <aaronbernstein@cox.net>
2014-02-24 15:40:59 -0700

    Update mysql_cli_cmds.sh

--- SQL/mysql_cli_cmds.sh
+++ SQL/mysql_cli_cmds.sh
@@ -1,12 +1,14 @@
 # External Commands
-mysql -h <host> -u <username> -p<password>
+# CONNECT TO DATABASE SERVER
+mysql -h <HOST> -u <USERNAME> -p<PASSWORD>
 
-mysqldump -h <host> -u <username> -p<password> <database> > <filename.sql.dump> or < <filename.sql.dump>
+# BACKUP/RESTORE DATABASE 
+mysqldump -h <HOST> -u <USERNAME> -p<PASSWORD> <DATABASE> > <filename.sql.dump> or < <filename.sql.dump>
 
 # Dump Schema w/o data
-mysqldump -d -u someuser -p mydatabase
+mysqldump -d -u <USERNAME> -p<PASSWORD> <DATABASE>
 
-mysqladmin -h <host> -u <username> -p<password> status
+mysqladmin -h <HOST> -u <USERNAME> -p<PASSWORD> status
 
 # Execute interal command from CLI
 mysql -u$user -p$pass -S /tmp/mysql.sock -e "SELECT @@version"
@@ -17,23 +19,45 @@ SHOW PROCESSLIST;
 SHOW STATUS LIKE '%onn%'
 SHOW STATUS LIKE 'Conn%'
 
-SHOW VARIABLES LIKE 'character_set%'
-SHOW VARIABLES LIKE 'collation%'
-
 SHOW MASTER STATUS;
 SHOW SLAVE STATUS;
 SHOW SLAVE HOSTS;
 
-SHOW CHARACTER SET;
-SHOW COLLATION;
 SHOW GRANTS;
 SHOW PRIVILEGES;
 
+SHOW VARIABLES LIKE 'character_set%'
+SHOW VARIABLES LIKE 'collation%'
+
+SHOW CHARACTER SET;
+SHOW COLLATION;
 
 ALTER TABLE <table> CONVERT TO CHARACTER SET <charset> COLLATE <collation>;
 
-# use information_schema;
-SELECT TABLE_SCHEMA, TABLE_NAME, ENGINE, TABLE_COLLATION FROM TABLES WHERE TABLE_SCHEMA IN (<SCHEMA NAME>);
+# RETRIEVE INFORMATION ON SPECIFIC DB/TABLES
+SET @TBL_NAME:='', @TBL_SCHEMA:='';
+SELECT 
+  TABLE_SCHEMA, TABLE_NAME, ENGINE, TABLE_COLLATION 
+FROM TABLES 
+WHERE TABLE_SCHEMA IN (@TBL_SCHEMA);
+
+SELECT 
+  update_time 
+FROM information_schema.`TABLES` T 
+WHERE TABLE_NAME=@TBL_NAME 
+  AND TABLE_SCHEMA=@TBL_SCHEMA;
+
+SELECT * 
+FROM information_schema.`TABLES` 
+WHERE TABLE_SCHEMA=@TBL_SCHEMA;
+
+# LIST ALL USERS AND BASIC ACCESS INFORMATION
+SELECT 
+  DISTINCT user, user.host, db 
+FROM mysql.user
+  JOIN db USING (User) 
+ORDER BY User DESC;
+
 
 # SELECT and Put Output to Var or File 
 SELECT ... INTO <var_list|OUTFILE|DUMPFILE>
